cd570ae4b662fb6b7bcaebdf6e6455a2c62b4cc8
Alexander <alex@heinventions.com>
2019-03-14 16:12:06 +0000

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requirements = ['tox', 'pytest', 'pytest-testdox', 'pytest-testdirectory', 
 
 setup(
     name='docnado',
-    version='1.0.7',
+    version='1.0.8',
     author='Hardy & Ellis Inventions LTD',
     author_email='support@heinventions.com',
 
