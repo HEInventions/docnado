00cdad9c5e5cce39c2063e9c35e206add5f50dbe
Alexander Wilson <alex@heinventions.com>
2022-02-22 13:23:18 +0000

    version increment

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ test_requirements = ['tox', 'pytest', 'pytest-testdox', 'pytest-testdirectory', 
 
 setup(
     name='docnado',
-    version='1.0.12',
+    version='1.0.13',
     author='Hardy & Ellis Inventions LTD',
     author_email='support@heinventions.com',
 
